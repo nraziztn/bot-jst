28bd5296e9296d8c8c2dfa8c5bfb674b613a6acb
nraziztn <95585565+nraziztn@users.noreply.github.com>
2021-12-06 21:52:08 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,31 +2,68 @@ var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
-const model = require('./sdk/model.js');
+const model = require('./sdk/model.js'); // predict
+const cls_model = require('./sdk/cls_model.js'); // cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5031223524:AAHdtW9pZ1hdwA3NqhpYYmofM3HxLxiNC8I'
 const bot = new TelegramBot(token, {polling: true});
 
-
+state = 0;
 // bots
 bot.onText(/\/start/, (msg) => { 
-    console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
+    state = 0;;
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+// input
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `masukan nilai i|v contohnya 9|9`
     );   
+    state = 1;
 });
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = parseFloat(s[0])
+        r = parseFloat(s[1])
+        
+        model.predict(
+            [
+                i, // string to float
+                r
+            ]
+         ).then((jres1)=>{
+            v = parseFloat(jres[0])
+            p = parseFloat(jres[1])
+                
+            cls_model.classify([i, r, v, p]).then((jres2)=>{
+                bot.sendMessage(
+                        msg.chat.id
+                        `nilai v yang diprediksi adalah ${jres1[0]} watt`
+                );
+                bot.sendMessage(
+                        msg.chat.id,
+                        `nilai p yang diprediksi adalah ${jres1[1]} watt`
+                );
+                bot.sendMessage(
+                        msg.chat.id,
+                        `Klasifikasi Tegangan $(jres2)`
+                );
+            })
+         })
+     }else{
+            state = 0
+     }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
@@ -37,6 +74,27 @@ r.get('/prediction/:i/:r', function(req, res, next) {
         ]
     ).then((jres)=>{
         res.json(jres);
+    })
+});
+
+// routers
+r.get('/classify/:i/:r', function(req, res, next) {    
+    model.predict(
+        [
+            parseFloat(req.params.i), // string to float
+            parseFloat(req.params.r)
+        ]
+    ).then((jres)=>{
+        cls_model.classify(
+            [
+                parseFloat(req.params.i), // string to float
+                parseFloat(req.params.r),
+                parseFloat(jres[0]),
+                parseFloat(jres[1])
+            ]
+        ).then((jres_)=>{
+            res.json(jres_)
+        })
     })
 });
 
